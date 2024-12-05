5c89fb31772ef05ba75b7a2b14a606040463c44f
Kieran911 <arinzekieran@gmail.com>
2024-12-05 08:57:10 +0100

    Object create

--- OOP-Constructors-&-Prototypes/object-create/script.js
+++ OOP-Constructors-&-Prototypes/object-create/script.js
@@ -1,19 +1,30 @@
-function Rectangle(width, height) {
-  this.width = width;
-  this.height = height;
+const rectanglePrototypes = {
+  area: function () {
+    return this.width * this.height;
+  },
+  perimeter: function () {
+    return 2 * (this.width + this.height);
+  },
+  isSquare: function () {
+    return this.height === this.width;
+  },
+};
+
+function createRectangle(height, width) {
+  return Object.create(rectanglePrototypes, {
+    height: {
+      value: height,
+    },
+    width: {
+      value: width,
+    },
+  });
 }
 
-Rectangle.prototype.area = function () {
-  return this.height * this.width;
-};
+const rect = createRectangle(10, 20);
+console.log(rect);
+console.log(rect.area());
+console.log(rect.isSquare());
 
-Rectangle.prototype.perimeter = function () {
-  return 2 * (this.height * this.width);
-};
-
-Rectangle.prototype.isSquare = function () {
-  return this.height === this.width;
-};
-
-const rectangle1 = new Rectangle(4, 4);
-console.log(rectangle1.isSquare(), rectangle1.area());
+const rect2 = createRectangle(20, 20);
+console.log(rect2.area());
