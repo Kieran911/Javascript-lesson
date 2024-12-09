9472f1aecdd61ed8bdf7f588ec3ed0a295587c7a
Kieran911 <arinzekieran@gmail.com>
2024-12-09 18:28:28 +0100

    Javascript Lessons

--- oop-classes-&-private-properties/property-flags-&-descriptors/script.js
+++ oop-classes-&-private-properties/property-flags-&-descriptors/script.js
@@ -2,3 +2,34 @@
 // [[Enumerable]] - if `true`, the property will be returned in a `for...in` loop, otherwise not
 // [[Writable]] - if `true`, the value of the property can be changed, otherwise not
 // [[Value]] - the value of the property
+
+Math.PI = 4;
+console.log(Math.PI);
+
+let descriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
+
+const rectObj = {
+  name: 'Rectangle 1',
+  width: 10,
+  hieght: 10,
+};
+
+Object.defineProperty(rectObj, 'name', {
+  writable: false,
+  configurable: false,
+  enumerable: false,
+});
+
+descriptor = Object.getOwnPropertyDescriptor(rectObj, 'name');
+console.log(descriptor);
+
+rectObj.name = 'New name';
+delete rectObj.name;
+
+console.log(rectObj);
+
+for (let [key, value] of Object.entries(rectObj)) {
+  console.log(`${key}: ${value}`); // Name isn't logged cos of the inumerable beign set to false
+}
+
+console.log(Object.getOwnPropertyDescriptors(rectObj));
