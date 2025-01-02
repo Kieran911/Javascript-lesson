85e887e315d7ab49d349ca316e4f0419a3528b3e
Kieran911 <arinzekieran@gmail.com>
2025-01-02 06:39:54 +0100

    Refactor set operations and enhance functionality in script.js

--- iterators-data-structures/sets/script.js
+++ iterators-data-structures/sets/script.js
@@ -1,22 +1,37 @@
-const set = new Set([1, 2, 2, 2, 2, 3, 4, 4, 4, 4]); // All the repitions will not show up on the console. the results will be [1, 2, 3, 4]
+const set = new Set([1, 2, 2, 3, 3, 4]);
 
+// Add a new value to the set
 set.add(5);
-
-console.log(set.has(3));
-console.log(set.has(6));
-
-set.delete(5);
-console.log(set.has(5));
 
 console.log(set);
 
-const setArray = Array.from(set);
-console.log(setArray);
+// Check for membership
+console.log(set.has(5));
+console.log(set.has(6));
 
+// Remove a value from the set
+set.delete(5);
+
+console.log(set);
+
+// Convert to array
+const array = Array.from(set);
+
+// Convert array to set
+const arraySet = new Set(array);
+
+// Get the size of the set
+console.log('Set Size:', set.size);
+
+// Get values from the set - returns an iterator
+console.log(set.values());
+
+// Iterate through the set using a loop
 for (let item of set) {
   console.log(item);
 }
 
+// use iterator
 const iterator = set.values();
 
 console.log(iterator.next());
@@ -25,6 +40,7 @@ console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
 
+// Clear the set
 set.clear();
 
 console.log(set);
